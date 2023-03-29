36d8eefc7186d86a4debb264e48fb67611315e1a
Raphael Barreiro <126487945+RBarreiro25@users.noreply.github.com>
2023-03-29 11:01:37 -0300

    V2
    The V1 it's log changes is on my private repository. Contact me if you need.

---
+++ changeImage.js
@@ -0,0 +1,91 @@
+// function to change the icon of the glass -CODE SMELL!-
+
+
+        function imagefun() {
+
+            var Image_Id = document.getElementById('getImage');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        }  
+        
+        function imagefun1() {
+
+            var Image_Id = document.getElementById('getImage1');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        }  
+
+        function imagefun2() {
+
+            var Image_Id = document.getElementById('getImage2');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        } 
+
+        function imagefun3() {
+
+            var Image_Id = document.getElementById('getImage3');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        } 
+
+        function imagefun4() {
+
+            var Image_Id = document.getElementById('getImage4');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        } 
+
+        function imagefun5() {
+
+            var Image_Id = document.getElementById('getImage5');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        } 
+
+        function imagefun6() {
+
+            var Image_Id = document.getElementById('getImage6');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        } 
+        function imagefun7() {
+
+            var Image_Id = document.getElementById('getImage7');
+
+            if (Image_Id.src.match("/assets/glasses/glass-empty.png")) {
+                Image_Id.src = "/assets/glasses/glass-full.png";
+            } else {
+                Image_Id.src = "/assets/glasses/glass-empty.png";
+            }
+        } 
+
+
